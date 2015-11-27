4bb6d37aaedf1ebeb770e561bea2f01e37843a55
Robert Brown <ammoknight@gmail.com>
2015-11-27 12:35:55 -0700

    Publish to Hex

--- mix.exs
+++ mix.exs
@@ -2,35 +2,41 @@ defmodule Monad.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :monadex,
-     version: "1.0.1",
-     elixir: "~> 1.0",
-     name: "MonadEx",
-     source_url: "https://github.com/rob-brown/MonadEx",
-     homepage_url: "https://github.com/rob-brown/MonadEx",
-     deps: deps]
+    [
+      app: :monadex,
+      version: "1.0.2",
+      elixir: "~> 1.0",
+      name: "MonadEx",
+      source_url: "https://github.com/rob-brown/MonadEx",
+      homepage_url: "https://github.com/rob-brown/MonadEx",
+      deps: deps,
+      description: description,
+      package: package,
+   ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.7", only: :dev},
+    ]
+  end
+
+  defp description do
+    """
+    Improve pipelines with monads.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Robert Brown"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/rob-brown/MonadEx"},
     ]
   end
 end
